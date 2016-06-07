db61dee388cfcb94ed4ac4792794e05f2a009197
tiangui-exc <tiangui@hohoxc.com>
2016-06-07 15:41:53 +0800

    add View

---
+++ demo/xlib/src/main/java/me/walkonly/lib/view/ListViewForScrollView.java
@@ -0,0 +1,30 @@
+package me.walkonly.lib.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class ListViewForScrollView extends ListView {
+
+    public ListViewForScrollView(Context context) {
+        super(context);
+    }
+
+    public ListViewForScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListViewForScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 让ListView嵌套于ScrollView时能正常显示
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+}
