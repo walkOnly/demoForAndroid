e56980c3e960d5431efba4af4ef9a0a5f0d5f948
tiangui-exc <tiangui@hohoxc.com>
2016-06-08 14:50:31 +0800

    add MainActivity

---
+++ demo/app/src/main/java/com/hhxc/demo/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.hhxc.demo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import me.walkonly.lib.activity.BaseActivity;
+import me.walkonly.lib.util.Tip;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private long mBackClickTime = 0;
+
+    @Override
+    public void onBackPressed() {
+        long now = System.currentTimeMillis();
+        if (now - mBackClickTime > 3000) {
+            Tip.show("再次点击后退键退出应用");
+            mBackClickTime = now;
+            return;
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
